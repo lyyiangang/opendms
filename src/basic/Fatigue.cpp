3d77135a3b516c225273d3c571637df4b86b6b6a
lyy <lyyiangang@sina.com>
2021-11-08 18:31:45 +0800

    test lint

--- src/basic/Fatigue.cpp
+++ src/basic/Fatigue.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 namespace opendms
 {
-    Fatigue::Fatigue():yawn(false)
+    Fatigue::Fatigue() : yawn(false)
     {
         eye_openness = {1, 1};
     }
@@ -11,37 +11,40 @@ namespace opendms
     {
     }
 
-    bool Fatigue::Process(const Frame& frame, const FaceData& face_data){
+    bool Fatigue::Process(const Frame &frame, const FaceData &face_data)
+    {
         yawn = false;
         eye_openness = {1, 1};
         cv::Mat landmarks = face_data.landmark;
-        if(face_data.found_face)
+        if (face_data.found_face)
         {
-            //yawn?     
-            float width = cv::norm(landmarks.row(48) - landmarks.row(54));
-            float height = cv::norm(landmarks.row(66) - landmarks.row(62));
-            if(height / width > 0.4){
+            // yawn?
+            float yawn_width = cv::norm(landmarks.row(48) - landmarks.row(54));
+            float yawn_height = cv::norm(landmarks.row(66) - landmarks.row(62));
+            if (yawn_height / yawn_width > 0.4)
+            {
                 yawn = true;
             }
-            // close eye? 
-            for(int ii = 0; ii <2; ++ii )
+            // close eye?
+            for (int ii = 0; ii < 2; ++ii)
             {
-                cv::Vec2i corner_idx={36, 39};
+                cv::Vec2i corner_idx = {36, 39};
                 cv::Vec2i up_idx = {37, 38};
                 cv::Vec2i down_idx = {41, 40};
-                if(ii == 1){
+                if (ii == 1)
+                {
                     corner_idx = {42, 45};
                     up_idx = {43, 44};
                     down_idx = {47, 46};
                 }
-                width = cv::norm(landmarks.row(corner_idx[0]) - landmarks.row(corner_idx[1]));
-                height = cv::norm((landmarks.row(up_idx[0]) + landmarks.row(up_idx[1])) / 2 , \
-                    (landmarks.row(down_idx[0]) + landmarks.row(down_idx[1])) / 2);
+                float width = cv::norm(landmarks.row(corner_idx[0]) - landmarks.row(corner_idx[1]));
+                float height = cv::norm((landmarks.row(up_idx[0]) + landmarks.row(up_idx[1])) / 2,
+                                        (landmarks.row(down_idx[0]) + landmarks.row(down_idx[1])) / 2);
                 lg->debug("left eye height:{}", height);
-                if(width > 1e-3)
+                if (width > 1e-3)
                     eye_openness[ii] = height / width;
             }
         }
         return true;
     }
-} 
+}
