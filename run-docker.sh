00bd5515db34ba3a81d3d151d6c0b958a357a293
lyy <lyyiangang@sina.com>
2021-11-09 13:25:14 +0800

    include .ssh in docker

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,9 @@
 xhost +local:${USER}
-#-u $(id -u):$(id -g) \
 docker run -it --rm     \
+    -u $(id -u) \
     -e DISPLAY=$DISPLAY     \
+    -v ${HOME}/.ssh:/.ssh \
+    -v ${HOME}/.ssh:/root/.ssh \
     -v /tmp/.X11-unix:/tmp/.X11-unix     \
     -v ${PWD}:${PWD} \
     -w ${PWD} \
